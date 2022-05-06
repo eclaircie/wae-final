8fee016dda91af9fcd5c713542f1c4c9d84c4cf3
ally <almfr00@gmail.com>
2022-05-06 16:39:39 -0400

    deleted unused comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
-import { MenuComponent } from './menu/menu.component';
 import { CouponComponent } from './coupon/coupon.component';
-import { CouponBlockComponent } from './coupon-block/coupon-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuItemComponent,
-    MenuComponent,
     CouponComponent,
-    CouponBlockComponent
   ],
   imports: [
     BrowserModule,
--- src/app/data/menu-items.ts
+++ src/app/data/menu-items.ts
@@ -6,6 +6,11 @@ export const menuitems = [
     },
     {
         "type":"drink",
+        "name":"Soda",
+        "price":"2.00"
+    },
+    {
+        "type":"drink",
         "name":"Meyer's Lemonade",
         "price":"2.50"
     },
@@ -13,6 +18,11 @@ export const menuitems = [
         "type":"alcohol",
         "name":"Bottle of Roscato Rosso Dolce",
         "price":"15.50"
+    },
+    {
+        "type":"alcohol",
+        "name":"Triangle Cocktail",
+        "price":"13.30"
     },
 
     {
@@ -29,5 +39,21 @@ export const menuitems = [
         "type":"pizza",
         "name":"Marinara Pizza",
         "price":"13.00"
+    },
+
+    {
+        "type":"pasta",
+        "name":"Penne Gargonzola with Chicken",
+        "price":"15.00"
+    },
+    {
+        "type":"pasta",
+        "name":"Pasta Fagioli al Forno",
+        "price":"13.00"
+    },
+    {
+        "type":"pasta",
+        "name":"Rigatoni with Sausage",
+        "price":"14.50"
     }
 ]
